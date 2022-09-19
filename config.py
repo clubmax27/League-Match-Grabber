8555077a21b34ea300f940d486516eac907a3fb4
Maxence Mathieu <Sans.Plague@gmail.com>
2022-09-19 19:33:38 +0200

    on va arreter d'utiliser cass hein

---
+++ config.py
@@ -0,0 +1,3 @@
+"""Config file for the Match Grabber"""
+
+API_TOKEN = "RGAPI-22498ded-752c-4ea9-8d99-997f412c70bd"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 TODO
 
-- Faire une config file avec le token API à ajouter dans le .gitignore
+- DONE/ Faire une config file avec le token API à ajouter dans le .gitignore
 - Créer le modèle de la base de donnée
 
 
@@ -13,6 +13,45 @@ TODO
 
 - Pour chaque joueur, trouver l'historique de ses matchs avec , et les ajouter à la bdd
 
- 
-- Pour chaque match, tirer les données du match avec GET_getMatch, les données des mastery avec GET_getChampionMastery et les ajouter dans la bdd
+
+- Pour chaque match, tirer les données du match avec GET_getMatch, les données des mastery
+  avec GET_getChampionMastery et les ajouter dans la bdd
 """
+
+
+import cassiopeia as cass
+
+import config
+
+
+def main():
+    """Entry point of the function"""
+
+    API_TOKEN = config.API_TOKEN
+    cass.set_riot_api_key(API_TOKEN)
+
+    REGION = "EUW"
+
+    unit_test_summoner_info()
+
+    bronze_2_league = cass.LeagueEntries(
+        region=REGION,
+        queue=cass.Queue.ranked_solo_fives,
+        tier=cass.Tier.bronze,
+        division=cass.Division.one,
+    )
+
+    print(bronze_2_league[0].summonerName)
+
+
+def unit_test_summoner_info():
+    """Unit test that displays summoner info"""
+
+    summoner = cass.get_summoner(name="Bartemul1us", region="EUW")
+    print(
+        f"{summoner.name} is level {summoner.level} and their current elo \
+in ranked solo/duo is {summoner.ranks[cass.data.Queue.ranked_solo_fives]}"
+    )
+
+
+main()
